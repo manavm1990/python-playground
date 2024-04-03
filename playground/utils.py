8f282ab4e5aaca81c9c0a60ad88997e397ac305a
Manav Misra <manavmisra@planaday.com>
2024-04-03 12:14:56 -0500

    feat: :sparkles: 'count words in file'
    Includes refactor ♻️.

--- playground/utils.py
+++ playground/utils.py
@@ -1,9 +1,19 @@
+import re
+from collections import Counter
 from collections.abc import Iterable
+from pathlib import Path
 
 
 def convert_to_uppercase(strings: list) -> list:
     """Given a list of trings, convert them all to UPPER CASE."""
     return [string.upper() for string in strings]
+
+
+def count_words_in_file(filename: str) -> dict:
+    # `with` automatically closes the file
+    with Path.open(Path(filename)) as file_contents:
+        words = re.findall(r"\w+", file_contents.read().lower())
+    return dict(Counter(words))
 
 
 def greet(name: str) -> str:
@@ -21,6 +31,10 @@ def greet_all(names: Iterable[str]) -> list:
     return [greet(name) for name in names]
 
 
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
+
+
 def normalize_id(user_id: int | str) -> str:
     if isinstance(user_id, int):
         return f"user-{100_000 + user_id}"
@@ -31,7 +45,3 @@ def normalize_id(user_id: int | str) -> str:
 def sum_even_numbers(numbers: Iterable[int]) -> int:
     """Given an iterable of integers, return the sum of all even numbers in the iterable."""
     return sum(num for num in numbers if num % 2 == 0)
-
-
-def is_palindrome(s: str) -> bool:
-    return s == s[::-1]
