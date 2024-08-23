0101fc1cb3462e4bf3412ed608475e2a1d2b3be9
Manav Misra <manav.misra@planaday.com>
2024-08-23 09:47:24 -0500

    feat: :sparkles:

---
+++ playground/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, initial_balance: float = 0.00) -> None:
+        self.balance: float = initial_balance
+
+    def deposit(self, amount: float) -> float:
+        self.balance += amount
+        return self.balance
+
+    def withdrawal(self, amount: float) -> float:
+        if amount <= self.balance:
+            self.balance -= amount
+        else:
+            raise ValueError("Insufficient Funds")
+        return self.balance
+
+    def get_balance(self) -> float:
+        return self.balance
