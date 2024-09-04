c2134872c916982cb3ed33645da9bfff43dfe7e3
Manav Misra <manav.misra@planaday.com>
2024-09-04 10:21:47 -0500

    test: :white_check_mark: add 'test_deposit'

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -5,3 +5,8 @@ bank_account = BankAccount(100.00)
 
 def test_get_balance():
     assert bank_account.get_balance() == 100.00
+
+
+def test_deposit():
+    bank_account.deposit(50.00)
+    assert bank_account.get_balance() == 150.00
