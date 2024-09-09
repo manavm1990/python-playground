a2f8ace0344d7a0e1db4f4c3aad872c1dc9c731b
Manav Misra <manav.misra@planaday.com>
2024-09-09 00:11:17 -0500

    test: :white_check_mark:

---
+++ tests/test_employee.py
@@ -0,0 +1,18 @@
+# tests/test_employee.py
+import pytest
+
+from playground.employee import Employee
+
+
+@pytest.fixture
+def employee():
+    return Employee(name="Jane Doe", emp_id=5678, monthly_salary=6000.0)
+
+
+def test_get_annual_salary(employee):
+    assert employee.get_annual_salary() == 72000.00
+
+
+def test_apply_raise(employee):
+    employee.apply_raise(raise_amount=1000.0)
+    assert employee.get_annual_salary() == 84000.00
