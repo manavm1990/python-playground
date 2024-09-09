a100ee224f61dc94e05189f715fceb3bd18a8348
Manav Misra <manav.misra@planaday.com>
2024-09-08 23:57:45 -0500

    feat: :necktie: add 'Employee'

---
+++ playground/employee.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self, name: str, emp_id: int, monthly_salary: float) -> None:
+        self.name = name
+        self.emp_id = emp_id
+        self.salary = monthly_salary
+
+    def annual_salary(self) -> float:
+        return self.salary * 12
+
+    def apply_raise(self, raise_amount: float) -> None:
+        self.salary += raise_amount
