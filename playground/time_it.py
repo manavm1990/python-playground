c306e7abf7b54ae2542268c023fb76b0fb882ea0
Manav Misra <manav.misra@planaday.com>
2024-09-11 00:56:24 -0500

    feat: :necktie: add 'time_it' decorator

---
+++ playground/time_it.py
@@ -0,0 +1,19 @@
+# time_it.py
+
+import time
+from collections.abc import Callable
+from typing import Any
+
+
+def time_it(
+    func: Callable[..., Any],
+) -> Callable[..., Any]:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Function `{func.__name__}` executed in {execution_time:.6f} seconds")
+        return result
+
+    return wrapper
