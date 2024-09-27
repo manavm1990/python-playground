de80f112a9cc4e19a3c8cd12056cd0d6f2087111
Manav Misra <manavm@visionwebsoft.com>
2024-09-26 23:37:11 -0500

    test: :fire:

--- tests/test_file_iterator.py
+++ tests/test_file_iterator.py
@@ -29,16 +29,3 @@ def test_file_iterator_strips_lines(temp_file):
 
     with pytest.raises(StopIteration):
         next(iterator)
-
-
-def test_file_iterator_using_example_file(temp_file):
-    file = temp_file("Hello, World! Hello")
-    file_iterator = FileIterator(file)
-    iterator = iter(file_iterator)
-
-    expected_lines = ["Hello, World! Hello"]
-    for expected_line in expected_lines:
-        assert next(iterator) == expected_line
-
-    with pytest.raises(StopIteration):
-        next(iterator)
